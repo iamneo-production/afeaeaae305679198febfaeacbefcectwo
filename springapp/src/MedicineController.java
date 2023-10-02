6bf4965bafe07b9161ceb2b432699653a992cf09
Iamneo <techadmin@iamneo.ai>
2023-10-02 13:27:15 +0000

    Backup Commit

---
+++ springapp/src/MedicineController.java
@@ -0,0 +1,8 @@
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicines")
+public class MedicineController {
+
+    // Implement POST and PUT methods here
+}
