c8fbacbeb8f75fdf53d3e7e7db7ce60f97c0e88c
Iamneo <techadmin@iamneo.ai>
2023-10-02 13:32:15 +0000

    Backup Commit

--- springapp/src/MedicineController.java
+++ springapp/src/MedicineController.java
@@ -1,8 +1,21 @@
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicines")
 public class MedicineController {
 
-    // Implement POST and PUT methods here
+    private List<Medicine> medicineList = new ArrayList<>();
+
+    @PostMapping
+    public boolean addMedicine(@RequestBody Medicine medicine) {
+        // Add logic to add medicine to the system
+        // Return true if successful, otherwise false
+    }
+
+    @PutMapping("/{medicineId}")
+    public Medicine updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
+        // Add logic to update medicine with given ID
+        // Return the updated Medicine object
+    }
 }
---
+++ springapp/src/model/Medicine.java
@@ -0,0 +1,10 @@
+public class Medicine {
+
+    private int medicineId;
+    private String medicineName;
+    private float price;
+    private int quantity;
+    private String description;
+
+    // Generate appropriate getters, setters, and constructors.
+}
